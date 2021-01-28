40384c4d63f5e5abeeca6f71a91e971439007eee
JoniJuntto <AA5513@student.jamk.fi>
2021-01-28 15:26:26 +0200

    Excercise 2 chapter 1 ready

---
+++ HelloThymeleaf/src/main/java/fi/haagahelia/course/web/MessageController.java
@@ -0,0 +1,32 @@
+package fi.haagahelia.course.web;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fi.haagahelia.course.domain.Message;
+
+@Controller
+public class MessageController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String greetingForm(
+
+			@RequestParam(name = "name") String name, @RequestParam(name = "age") String age, Model model){
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "hello";
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public String greetingSubmit(@ModelAttribute Message msg, Model model) {
+		model.addAttribute("message", msg);
+		return "result";
+	}
+}
