fc225e73c42835d01bbb86ba72e6059332847d13
JoniJuntto <AA5513@student.jamk.fi>
2021-02-01 12:14:41 +0200

    Added bookstore folder

---
+++ HelloThymeleaf/src/main/java/fi/haagahelia/course/domain/Student.java
@@ -0,0 +1,29 @@
+package fi.haagahelia.course.domain;
+
+public class Student {
+	
+	private String FirstName;
+	private String LastName;
+	
+	
+	
+	public Student(String firstName, String lastName) {
+		super();
+		FirstName = firstName;
+		LastName = lastName;
+	}
+
+
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+}
---
+++ HelloThymeleaf/src/main/java/fi/haagahelia/course/web/StudentController.java
@@ -0,0 +1,45 @@
+package fi.haagahelia.course.web;
+
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import fi.haagahelia.course.domain.*;
+import fi.haagahelia.course.domain.Message;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+	@RequestMapping(value = "/studentlist", method = RequestMethod.GET)
+		public String showStudentList(Model model) {
+		List<Student> students = new ArrayList<Student>();
+		
+		Student student1 = new Student("Joni", "Juntto");
+		Student student2 = new Student("Mari", "Pelkonen");
+		Student student3 = new Student("Janne", "Hyypiä");
+		
+		students.add(student1);
+		students.add(student2);
+		students.add(student3);
+		
+		model.addAttribute("students", students);
+		
+			return "studentlist";
+		}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+		public String greetingSubmit(@ModelAttribute Message msg, Model model) {
+			model.addAttribute("message", msg);
+			return "result";
+		}
+}
